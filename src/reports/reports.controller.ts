83fcb6f03c407a6d17481385cf3254ea4969cb07
webcreation-dev <adjilan2403@gmail.com>
2024-11-08 19:44:23 +0100

    adding relation with user and report

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards } from '@nestjs/common';
+import { CreateReportDto } from './dtos/create-report-dto';
+import { ReportsService } from './reports.service';
+import { AuthGuard } from 'src/guards/auth.guard';
+import { Report } from './report.entity';
+import { CurrentUser } from 'src/users/decorators/current-user.decorator';
+import { User } from 'src/users/user.entity';
+import { Serialize } from 'src/interceptors/serialize.interceptor';
+import { ReportDto } from './dtos/report.dto';
 
-@Controller('reports')
-export class ReportsController {}
+@Controller('/reports')
+@Serialize(ReportDto)
+export class ReportsController {
+  constructor(private reportService: ReportsService) {}
+
+  @Post('/')
+  @UseGuards(AuthGuard)
+  createReport(
+    @Body() body: CreateReportDto,
+    @CurrentUser() user: User,
+  ): Promise<Report> {
+    return this.reportService.create(body, user);
+  }
+}
--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateReportDto } from './dtos/create-report-dto';
+import { Report } from './report.entity';
+import { User } from 'src/users/user.entity';
 
 @Injectable()
-export class ReportsService {}
+export class ReportsService {
+  constructor(@InjectRepository(Report) private repo: Repository<Report>) {}
+
+  create(reportDto: CreateReportDto, user: User): Promise<Report> {
+    const report = this.repo.create(reportDto);
+    report.user = user;
+    return this.repo.save(report);
+  }
+}
